863c4a2d28e5b4c9eb39d56f821f6ddc0e812406
100 Pixels Squared <49565664+100PXSquared@users.noreply.github.com>
2021-02-24 05:08:21 +0000

    Add timestamp to root messages

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const messageContainer = document.getElementById("message-container")
 const messageTemplate = `
 <img class="avatar" src="{AVATAR}">
 <div class="content">
-	<span class="username">{USERNAME}</span>
+	<div class="message-header"><span class="username">{USERNAME}</span><span class="timestamp">{TIMESTAMP}</span></div>
 	<div class="message-text">{CONTENT}</div>
 </div>
 `
@@ -13,14 +13,15 @@ const concatMessageTemplate = `
 	<div class="message-text">{CONTENT}</div>
 </div>
 `
-let lastUser = {"id": "", "avatar": "", "name": ""}
+let lastUser = {"id": "", "avatar": "", "name": "", "time": 0}
 function addMessage(id, avatar, name, text) {
 	const message = document.createElement("div")
+	const timestamp = new Date()
 	
-	if (id != lastUser.id || avatar != lastUser.avatar || name != lastUser.name) {
+	if (id != lastUser.id || timestamp.getTime() - lastUser.time > 420000 || avatar != lastUser.avatar || name != lastUser.name) {
 		message.className = "message"
-		message.innerHTML = messageTemplate.replace("{AVATAR}", avatar).replace("{USERNAME}", name).replace("{CONTENT}", text)
-		lastUser = {"id": id, "avatar": avatar, "name": name}
+		message.innerHTML = messageTemplate.replace("{AVATAR}", avatar).replace("{USERNAME}", name).replace("{TIMESTAMP}", timestamp.toLocaleString()).replace("{CONTENT}", text)
+		lastUser = {"id": id, "avatar": avatar, "name": name, "time": timestamp.getTime()}
 	} else {
 		message.className = "message concat"
 		message.innerHTML = concatMessageTemplate.replace("{CONTENT}", text)

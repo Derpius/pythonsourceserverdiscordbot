16e27ac5dd6fd87bb2ffddd94b6c5c01ba964c77
Derpius <49565664+Derpius@users.noreply.github.com>
2021-09-21 11:44:12 +0100

    Implement GMod client InfoPayload funcs

--- gmod_addon/lua/relay/infopayload.lua
+++ gmod_addon/lua/relay/infopayload.lua
@@ -1,70 +1,131 @@
-local string_format = string.format
-local _rawget, _ipairs, _setmetatable = rawget, ipairs, setmetatable
+local members, roles, emotes = {}, {}, {}
+
+local string_format, string_find, string_lower = string.format, string.find, string.lower
+local _rawget, _setmetatable = rawget, setmetatable
+local _ipairs, _pairs = ipairs, pairs
 local _error = error
+local table_insert, table_sort = table.insert, table.sort
+
+/*
+	Netcode
+*/
+if SERVER then
+
+else
+
+end
 
 /*
 	InfoPayload funcs
 */
+local function sortedFind(tbl, match)
+	// Get matches
+	local matches = {}
+	for k, v in _pairs(tbl) do
+		local weight = match(v)
+		if weight then table_insert(matches, {weight, v}) end
+	end
+
+	// Sort
+	table_sort(matches, function(a, b) return a[1] < b[1] end)
+
+	// Clean table
+	for i, v in _ipairs(matches) do matches[i] = matches[i][2] end
+
+	return matches
+end
+
 // Members
 // Get the entire member table
 function DiscordRelay.GetMembers()
-	return {}
+	return members
 end
 local getMembers = DiscordRelay.GetMembers
 
 // Get a member by id
 function DiscordRelay.GetMember(id)
-	return nil
+	return members[id]
 end
 local getMember = DiscordRelay.GetMember
 
-// Get a list of members who match the given name
+// Get a list of members who match the given name (sorted by importance descending)
 function DiscordRelay.FindMembersByName(name, caseSensitive)
 	if caseSensitive == nil then caseSensitive = false end
+	if not caseSensitive then name = string_lower(name) end
 
-	return {}
+	return sortedFind(members, function(member)
+		local matchIdx = string_find(
+			caseSensitive and member:GetDisplayName() or string_lower(member:GetDisplayName()),
+			name
+		)
+
+		if not matchIdx then
+			matchIdx = string_find(
+				caseSensitive and member:GetUsername() or string_lower(member:GetUsername()),
+				name
+			)
+
+			if matchIdx then
+				return matchIdx * 10 // Heavily weight only username matches to the end
+			end
+		end
+
+		return matchIdx
+	end)
 end
 local findMembersByName = DiscordRelay.FindMembersByName
 
 // Roles
 // Get the entire roles table
 function DiscordRelay.GetRoles()
-	return {}
+	return roles
 end
 local getRoles = DiscordRelay.GetRoles
 
 // Get a role by id
 function DiscordRelay.GetRole(id)
-	return nil
+	return roles[id]
 end
 local getRole = DiscordRelay.GetRole
 
-// Get a list of roles that match the given name
+// Get a list of roles that match the given name (sorted by importance descending)
 function DiscordRelay.FindRolesByName(name, caseSensitive)
 	if caseSensitive == nil then caseSensitive = false end
+	if not caseSensitive then name = string_lower(name) end
 
-	return {}
+	return sortedFind(roles, function(role)
+		return string_find(
+			caseSensitive and role:GetName() or string_lower(role:GetName()),
+			name
+		)
+	end)
 end
 local findRolesByName = DiscordRelay.FindRolesByName
 
 // Emotes
 // Get the entire emote table
 function DiscordRelay.GetEmotes()
-	return {}
+	return emotes
 end
 local getEmotes = DiscordRelay.GetEmotes
 
 // Get an emote by id
 function DiscordRelay.GetEmote(id)
-	return nil
+	return emotes[id]
 end
 local getEmote = DiscordRelay.GetEmote
 
-// Get a list of emotes that match the given name
+// Get a list of emotes that match the given name (sorted by importance descending)
 function DiscordRelay.FindEmotesByName(name, caseSensitive)
 	if caseSensitive == nil then caseSensitive = false end
+	if not caseSensitive then name = string_lower(name) end
 
-	return {}
+	return sortedFind(emotes, function(emote)
+		return string_find(
+			caseSensitive and emote:GetName() or string_lower(emote:GetName()),
+			name
+		)
+	end)
 end
 local findEmotesByName = DiscordRelay.FindEmotesByName
 

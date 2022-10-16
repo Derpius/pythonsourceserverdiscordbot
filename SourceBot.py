5855c57338ad7280a1152914916e5bd515698ff4
Derpius <49565664+Derpius@users.noreply.github.com>
2022-10-16 21:32:45 +0100

    Add status print before running the command

--- SourceBot.py
+++ SourceBot.py
@@ -526,6 +526,7 @@ async def restart(ctx: Context, configure: str = None):
 		await ctx.reply(f"A restart command is not configured for this channel, use `{config.prefix}restart configure [command]` to set one up")
 		return
 
+	await ctx.reply("Running restart command...")
 	result = subprocess.run(restartCmd, shell=True, capture_output=True, encoding="utf-8")
 
 	output = "No output"
